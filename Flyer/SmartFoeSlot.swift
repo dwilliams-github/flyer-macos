a7a771963230ae9ffb127b1d417180345be11044
David Williams <dwilliams@slashdave.com>
2020-01-11 14:31:30 -0800

    Badder foes appear with increasing frequency over play time

--- Flyer/SmartFoeSlot.swift
+++ Flyer/SmartFoeSlot.swift
@@ -13,6 +13,7 @@ class SmartFoeSlot: FoeSlot {
     private var box: Box
     private var mine: Mine
     private var smartMine: SmartMine
+    private var generation: Int
 
     override init( scene: SKScene, player: Player, delay: TimeInterval ) {
         
@@ -23,6 +24,8 @@ class SmartFoeSlot: FoeSlot {
         box = Box( scene: scene )
         mine = Mine( scene: scene)
         smartMine = SmartMine( scene: scene )
+        
+        generation = 0
 
         super.init(scene: scene, player: player, delay: delay)
     }
@@ -30,8 +33,17 @@ class SmartFoeSlot: FoeSlot {
     override func wakeUp() {
         //
         // Select foe from our four foe types
+        //  * 1/4 of the time we get a mine
+        //  * Some fraction of the time we get a badder version
         //
-        let badder = CGFloat.random(in: 0 ..< 1) > 0.75
+        // Badder fraction is zero at n = 0, increases
+        // to 1/4 at n = 50, and asymptotically approaches 3/4
+        // at n = infinity
+        //
+        let floatGeneration = CGFloat(self.generation)
+        let badderFraction = 0.75 * floatGeneration / (50.0 + floatGeneration)
+        
+        let badder = CGFloat.random(in: 0 ..< 1) < badderFraction
 
         if CGFloat.random(in: 0 ..< 1) < 0.25 {
             active = badder ? smartMine : mine
@@ -39,6 +51,8 @@ class SmartFoeSlot: FoeSlot {
         else {
             active = badder ? meanie : box
         }
+        
+        self.generation += 1
 
         active!.spawn( start: wakeUpPoint(), direction: wakeUpDirection(), difficulty: settings.difficulty )
     }
