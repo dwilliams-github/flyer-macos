2e8fc0b5a72f1904c6c6c79df4c0a0582b3ef866
David Williams <dwilliams@slashdave.com>
2020-01-20 23:53:18 -0800

    Make web site responsive

---
+++ Website/static/main.js
@@ -0,0 +1,8 @@
+function ShowMenu() {
+    let style = document.getElementById("menu").style;
+    if (style.display == "block") {
+        style.display = "none";
+    } else {
+        style.display = "block";
+    }
+}
